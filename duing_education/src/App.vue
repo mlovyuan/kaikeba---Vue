f71cc7516e18d7c2ec5d990b140b00c79ad45dc0
Yuan <mlovyuan@yahoo.com.tw>
2020-09-27 23:04:55 +0800

    2020.09.07_LearningSchedule

---
+++ duing_education/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Item :isActive="true">
+      123132
+    </Item>
+  </div>
+</template>
+
+<script>
+import Item from "./components/Item"
+export default {
+  name: 'App',
+  components: {
+    Item
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ duing_education/src/components/Item.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="item" :class="{active:true}">
+    <slot> </slot>
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        isActive:{
+            type: Boolean, // 約束isActive類型
+            // required: true, // 約束isActive是否為必填，必須要傳遞
+            default:false // 傳遞的默認值
+        }
+    },
+};
+</script>
+
+<style scoped>
+.active {
+    background: #e7e7e7;
+}
+.item {
+  cursor: pointer;
+  width: 100%;
+  height: 100%;
+}
+.item:hover {
+  background: #f4f4f4;
+}
+</style>
